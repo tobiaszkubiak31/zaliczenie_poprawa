4074cfe8d69f2ab906314916e9420ab1f72fc189
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-06-18 17:33:34 +0200

    Inital test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,11 +2,24 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+import java.util.Locale;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class ATMachineTest {
+
+    @Mock
+    private Bank bank;
 
     @BeforeEach
     public void setUp() throws Exception {}
@@ -15,5 +28,28 @@ public class ATMachineTest {
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
     }
+
+    private ATMachine atMachine;
+    @BeforeEach
+    void beforeEach() {
+        atMachine = new ATMachine(bank,Currency.getInstance(Locale.GERMANY));
+    }
+
+    @Test
+    void successMoneyDeposit(){
+        List<BanknotesPack> banknotesPackList = new ArrayList<BanknotesPack>();
+        banknotesPackList.add(BanknotesPack.create(2,Banknote.PL_20));
+        banknotesPackList.add(BanknotesPack.create(2,Banknote.PL_10));
+
+        MoneyDeposit deposit = MoneyDeposit.create(Currency.getInstance(Locale.GERMANY),banknotesPackList);
+
+
+
+
+
+
+    }
+
+
 
 }
